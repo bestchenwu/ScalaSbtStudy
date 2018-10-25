50ac3ceaeb9f2ff63f158d769c29fe383ada8bca
bestchenwu <bestchenwu@163.com>
2018-10-25 23:13:09 +0800

    提交actor

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,4 +1,17 @@
+import akka.actor.{Actor, ActorSystem, Props}
 
-object Main extends App{
-	println("hello world")
+class HelloActor(name:String) extends Actor {
+  override def receive: Receive = {
+    case "hello" => println(s"hello actor,$name")
+    case _ => println(s"huh?,$name")
+  }
+}
+
+object Main extends App {
+    val system = ActorSystem("HelloSystem")
+    //val helloActor = system.actorOf(Props[HelloActor],name="helloActor")
+    val helloActor = system.actorOf(Props(new HelloActor("sweet")),name="helloActor")
+    helloActor ! "hello"
+    helloActor ! "buenos"
+    system.terminate()
 }
