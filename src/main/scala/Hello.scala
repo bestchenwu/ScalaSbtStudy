1564f5f23ec3b50ba339c9be3898edd1c09207e9
chenwu <chenwu@meitunmama.com>
2018-10-26 19:54:56 +0800

    actor之间互相通信

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,10 +1,38 @@
 import akka.actor.{Actor, ActorSystem, Props}
 
 class HelloActor(name:String) extends Actor {
+
+
+  override def preStart(): Unit = {
+    println("before start helloActor")
+    super.preStart()
+  }
+
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    println("pre restart"+message.get)
+    super.preRestart(reason, message)
+  }
+
+
+  override def postRestart(reason: Throwable): Unit = {
+    println("post restart")
+    super.postRestart(reason)
+  }
+
+  override def postStop(): Unit = {
+    println("stoped helloActor")
+    super.postStop()
+  }
+
   override def receive: Receive = {
     case "hello" => println(s"hello actor,$name")
     case _ => println(s"huh?,$name")
   }
+}
+
+case object ForceRestart {
+  override def toString: String = {"forceRestart"}
 }
 
 object Main extends App {
@@ -12,6 +40,11 @@ object Main extends App {
     //val helloActor = system.actorOf(Props[HelloActor],name="helloActor")
     val helloActor = system.actorOf(Props(new HelloActor("sweet")),name="helloActor")
     helloActor ! "hello"
-    helloActor ! "buenos"
+   Thread.sleep(1000)
+  //helloActor ! ForceRestart
+
+  Thread.sleep(1000)
+   system.stop(helloActor)
+    //helloActor ! "buenos"
     system.terminate()
 }
---
+++ src/main/scala/unitThirteen/PingPongTest.scala
@@ -0,0 +1,57 @@
+package unitThirteen
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+case object StartMessage
+case object StopMessage
+case object PingMessage
+case object PoneMessage
+
+class PingActor(pone:ActorRef) extends Actor{
+
+  var count = 0;
+
+  def increment={
+    count+=1;
+  }
+
+  override def receive: Receive = {
+    case StartMessage => {
+      pone ! PingMessage
+      //this.increment
+    }
+    case PoneMessage =>{
+       if(count>2){
+         println(s"pone count is $count,now stop")
+        //结束ping pone信息传递
+         sender ! StopMessage
+         context.stop(self)
+       }else{
+          println("pone")
+          this.increment
+         pone ! PingMessage
+       }
+    }
+    case _=>println("PingActor can't understand:"+_)
+  }
+}
+
+class PoneActor extends Actor{
+  override def receive: Receive = {
+    case StopMessage=>{
+       println("now recieve Stop Message from PingActor")
+    }
+    case PingMessage =>{
+      println("ping")
+      sender ! PoneMessage
+    }
+    case _=>println("PoneActor can't understand:"+_)
+  }
+}
+
+object PingPongTest extends App{
+    val system = ActorSystem("pingPoneSystem")
+    val poneActor = system.actorOf(Props[PoneActor],"poneActor")
+    val pingActor = system.actorOf(Props(new PingActor(pone = poneActor)),"pingActor")
+    pingActor ! StartMessage
+}
