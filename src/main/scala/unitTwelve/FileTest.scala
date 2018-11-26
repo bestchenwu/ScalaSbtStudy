641b24f44d2913a659610255b724696f2da47594
chenwu <chenwu@meitunmama.com>
2018-11-26 20:27:04 +0800

    提交file的学习

--- src/main/scala/unitTwelve/FileTest.scala
+++ src/main/scala/unitTwelve/FileTest.scala
@@ -1,14 +1,25 @@
 package unitTwelve
 
+import java.io.{FileNotFoundException, IOException}
+
 import scala.io.Source
 
-object FileTest extends App{
+object FileTest extends App {
 
   //val flieName = "D:\\资料\\美囤妈妈\\BI\\babytree_git\\ScalaSbtStudy\\src\\main\\resources\\data.txt"
-  val flieName="/data/problem/problem.txt"
-  val bufferedSource = Source.fromFile(flieName)
-  for(line <- bufferedSource.getLines()){
-     println(line)
-   }
+  //  val flieName="/data/problem/problem.txt"
+  //  val bufferedSource = Source.fromFile(flieName)
+  //  for(line <- bufferedSource.getLines()){
+  //     println(line)
+  //   }
+  //
+  val fileName = "no-such.scala"
+  try {
+    var bufferedSource = Source.fromFile(fileName, "UTF-8")
+  } catch {
+    case e: FileNotFoundException => println("can't find file,for:" + e.getMessage)
+
+    case e: IOException => println("IOException,for:" + e.getMessage)
+  }
   //bufferedSource.close()
 }
