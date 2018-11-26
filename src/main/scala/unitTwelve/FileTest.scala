bfd8b2ce62b2e5b0bd0ac4724eefb72562c27ff1
bestchenwu <bestchenwu@163.com>
2018-11-26 23:03:24 +0800

    增加file字符流的读写

--- src/main/scala/unitTwelve/FileTest.scala
+++ src/main/scala/unitTwelve/FileTest.scala
@@ -1,25 +1,38 @@
 package unitTwelve
 
-import java.io.{FileNotFoundException, IOException}
+import java.io.{File, FileNotFoundException, IOException}
 
 import scala.io.Source
 
 object FileTest extends App {
 
   //val flieName = "D:\\资料\\美囤妈妈\\BI\\babytree_git\\ScalaSbtStudy\\src\\main\\resources\\data.txt"
-  //  val flieName="/data/problem/problem.txt"
-  //  val bufferedSource = Source.fromFile(flieName)
-  //  for(line <- bufferedSource.getLines()){
-  //     println(line)
-  //   }
-  //
-  val fileName = "no-such.scala"
-  try {
-    var bufferedSource = Source.fromFile(fileName, "UTF-8")
-  } catch {
-    case e: FileNotFoundException => println("can't find file,for:" + e.getMessage)
+  val flieName = "/data/problem/problem.txt"
+  val bufferedSource = Source.fromFile(flieName)
+  val newLine:Char = '\n'.toChar
+  var lineCount = 0
+  //这种语法称之为for循环的过滤表达式,或者称为卫语句
+  for{
+      line <- bufferedSource.getLines()
+      char <- line
+      if(char.equals(newLine))
+  } lineCount+=1
+  println(lineCount)
+//  for (line <- bufferedSource.getLines()) {
+//    println(line)
+//  }
+  //默认使用的就是char的循环器,如果文件很大，则处理速度相当慢
+//  for(char <- bufferedSource){
+//     println(char)
+//  }
 
-    case e: IOException => println("IOException,for:" + e.getMessage)
-  }
+  //  val fileName = "no-such.scala"
+  //  try {
+  //    var bufferedSource = Source.fromFile(fileName, "UTF-8")
+  //  } catch {
+  //    case e: FileNotFoundException => println("can't find file,for:" + e.getMessage)
+  //
+  //    case e: IOException => println("IOException,for:" + e.getMessage)
+  //  }
   //bufferedSource.close()
 }
