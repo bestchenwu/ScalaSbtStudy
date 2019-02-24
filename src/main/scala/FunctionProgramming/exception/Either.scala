9b1978237a1509ea54c7abf4a7f9da20da3db5b7
bestchenwu <bestchenwu@163.com>
2019-02-24 22:33:06 +0800

    提交either学习

--- src/main/scala/FunctionProgramming/exception/Either.scala
+++ src/main/scala/FunctionProgramming/exception/Either.scala
@@ -1,6 +1,27 @@
 package FunctionProgramming.exception
 
-sealed trait Either[+A,+E]
+sealed trait Either[+E,+A]{
+
+  def map[B](f:A=>B):Either[E,B]={
+      this match{
+        case Right(value)=>Right(f(value))
+        case Left(error)=>Left(error)
+      }
+  }
+
+  /**
+    * EE必须是E类型或者是E的父类型
+    *
+    * @param f
+    * @tparam EE
+    * @tparam B
+    * @return
+    */
+  def flatMap[EE>:E,B](f:A=>Either[EE,B]):Either[EE,B]={
+      //todo:
+      null
+  }
+}
 
 /**
   * left表示失败结果,value表示失败的描述
@@ -9,7 +30,7 @@ sealed trait Either[+A,+E]
   * @tparam E
   * @author chenwu on 2019.2.24
   */
-case class Left[+E](value:E) extends Either[Nothing,E]
+case class Left[+E](value:E) extends Either[E,Nothing]
 
 /**
   * right表示正确结果,value表示正确的值
@@ -18,7 +39,7 @@ case class Left[+E](value:E) extends Either[Nothing,E]
   * @tparam A
   * @author chenwu on 2019.2.24
   */
-case class Right[+A](value:A) extends Either[A,Nothing]
+case class Right[+A](value:A) extends Either[Nothing,A]
 
 object Either{
 
@@ -29,4 +50,6 @@ object Either{
       Right(xs.sum/xs.size)
     }
   }
+
+
 }
