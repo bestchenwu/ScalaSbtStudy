9589eec2568f3c35789433fedf7b786cba18df08
chenwu <chenwu@meitunmama.com>
2019-04-12 12:14:47 +0800

    提交ST

--- src/main/scala/FunctionProgramming/localeffects/ST.scala
+++ src/main/scala/FunctionProgramming/localeffects/ST.scala
@@ -27,6 +27,10 @@ object ST {
       override protected def run(s: S): (A, S) = (memo, s)
     }
   }
+
+  def runST[A](st: RunnableST[A]): A = st.apply[Unit].run(())._1
+
+
 }
 
 sealed trait STRef[S, A] {
@@ -40,6 +44,8 @@ sealed trait STRef[S, A] {
       ((), s)
     }
   }
+
+
 }
 
 object STRef {
@@ -48,4 +54,69 @@ object STRef {
   })
 }
 
+trait RunnableST[A] {
+  def apply[S]: ST[S, A]
+}
+
+/**
+  * Manifest用于存储运行时候A的信息<br/>
+  * 例如def foo[T](x:List[T])(implict m:Manifest[T])
+  * 这样当调用foo(List("a","b"),编译器就可以根据"a","b"推断出t类型是string
+  * 这里可以简写为def foo[T:Manifest](x:List[T])
+  *
+  * @param ev$1
+  * @tparam S
+  * @tparam A
+  */
+sealed abstract class STArray[S, A: Manifest] {
+  protected def value: Array[A]
+
+  def size: ST[S, Int] = ST(value.size)
+
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+    override protected def run(s: S): (Unit, S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def freeze: ST[S, List[A]] = ST(value.toList)
+
+  def fill(xs: Map[Int, A]): ST[S, Unit] = new ST[S, Unit] {
+    override protected def run(s: S): (Unit, S) = {
+      for ((index, item) <- xs) {
+        value(index) = item
+      }
+      ((), s)
+    }
+  }
+
+  def swap[S](i: Int, j: Int): ST[S, Unit] = for {
+    x <- read(i)
+    y <- read(j)
+    _ <- write(i, y)
+    _ <- write(j, x)
+  } yield ()
+
+//  def partition[S](arr:STArray[S,Int],n:Int,r:Int,pivot:Int):ST[S,Int]={
+//      val pivotValue = arr.read(pivot)
+//      val j = n
+//      for(i <- n to r){
+//          if(arr.read(i))
+//      }
+//  }
+
+}
+
+object STArray {
+  def apply[S, A: Manifest](size: Int, a: A): ST[S, STArray[S, A]] = ST(new STArray[S, A] {
+    lazy val value = Array(a)
+  })
+
+  def fromList[S, A: Manifest](xs: List[A]): ST[S, STArray[S, A]] = ST(new STArray[S, A] {
+    lazy val value = xs.toArray
+  })
+}
 
--- src/main/scala/FunctionProgramming/localeffects/STTest.scala
+++ src/main/scala/FunctionProgramming/localeffects/STTest.scala
@@ -2,8 +2,34 @@ package FunctionProgramming.localeffects
 
 object STTest {
   def main(args: Array[String]): Unit = {
-    val st = ST(20)
-    val newSt = st.map(_ * 2)
-    print(newSt)
+    //    val st = ST(20)
+    //    val newSt = st.map(_ * 2)
+    //    print(newSt)
+    //    val result = new RunnableST[(Int, Int)] {
+    //      override def apply[Int] = {
+    //        for {
+    //          r1 <- STRef(2)
+    //          r2 <- STRef(5)
+    //          a <- r1.read
+    //          b <- r2.read
+    //          _ <- r1.write(b * 2)
+    //          _ <- r2.write(a * 2)
+    //          a <- r1.read
+    //          b <- r2.read
+    //        } yield (a, b)
+    //      }
+    //    }
+    //
+    //    val result1 = ST.runST(result)
+    //    println(result1)
+    //val stArray = STArray(size=5,a=8)
+    //ST.runST(new RunnableST[STArray[S, A]] {})
+    //    val stArray = new STArray[Int, Int] {
+    //      override protected def value: Array[Int] = Array(1, 2, 3)
+    //    }
+    val stArray = STArray.fromList(List("a", "c", "b"))
+//    ST.runST(new RunnableST[STArray[String, String]] {
+//      override def apply[S]: ST[S, STArray[String, String]] = STArray.fromList(List("a", "c", "b"))
+//    })
   }
 }
