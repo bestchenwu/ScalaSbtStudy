d15220ded1af0f5e42bc3e0dbcc8bac8b166348c
chenwu <chenwu@meitunmama.com>
2019-04-18 16:55:55 +0800

    提交IO

--- src/main/scala/FunctionProgramming/streamingio/ExtendableIO.scala
+++ src/main/scala/FunctionProgramming/streamingio/ExtendableIO.scala
@@ -1,5 +1,25 @@
 package FunctionProgramming.streamingio
 
-trait ExtendableIO[F[_],O] {
+import scala.util.Try
 
+trait ExtendableIO[F[_], O] {
+
+}
+
+object ExtendableIO {
+
+  case class Await[F[_], A, O](req: F[A], recv: Either[Throwable, A] => ExtendableIO[F, O]) extends ExtendableIO[F, O]
+
+  case class Emit[F[_], O](head: O, tail: ExtendableIO[F, O]) extends ExtendableIO[F, O]
+
+  case class Halt[F[_], O](err: Throwable) extends ExtendableIO[F, O]
+
+  case object End extends Exception
+
+  case object Kill extends Exception
+
+//  def onHalt[F[_], O](f: Throwable => ExtendableIO[F, O]): ExtendableIO[F, O] = this match {
+//    //case Halt(e) => Try(f(e))
+//    case Emit(h,t)=>Emit(h,t.onHalt(f))
+//  }
 }
