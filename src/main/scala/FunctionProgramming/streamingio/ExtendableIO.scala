e53288dea91b2a381b42a15326b13d031f2c9fef
chenwu <chenwu@meitunmama.com>
2019-04-18 18:19:02 +0800

    提交extendable

--- src/main/scala/FunctionProgramming/streamingio/ExtendableIO.scala
+++ src/main/scala/FunctionProgramming/streamingio/ExtendableIO.scala
@@ -1,25 +1,29 @@
 package FunctionProgramming.streamingio
 
+import FunctionProgramming.streamingio.ExtendableIO.{Emit, ExtendableEmit, ExtendableHalt}
+
 import scala.util.Try
 
 trait ExtendableIO[F[_], O] {
 
+  def onHalt[F[_], O](f: Throwable => ExtendableIO[F, O]): ExtendableIO[F, O] = this match {
+    case ExtendableHalt(e) => f(e)
+    case ExtendableEmit(h,t)=>ExtendableEmit(h,t.onHalt(f))
+      
+  }
 }
 
 object ExtendableIO {
 
-  case class Await[F[_], A, O](req: F[A], recv: Either[Throwable, A] => ExtendableIO[F, O]) extends ExtendableIO[F, O]
+  case class ExtendableAwait[F[_], A, O](req: F[A], recv: Either[Throwable, A] => ExtendableIO[F, O]) extends ExtendableIO[F, O]
 
-  case class Emit[F[_], O](head: O, tail: ExtendableIO[F, O]) extends ExtendableIO[F, O]
+  case class ExtendableEmit[F[_], O](head: O, tail: ExtendableIO[F, O]) extends ExtendableIO[F, O]
 
-  case class Halt[F[_], O](err: Throwable) extends ExtendableIO[F, O]
+  case class ExtendableHalt[F[_], O](err: Throwable) extends ExtendableIO[F, O]
 
   case object End extends Exception
 
   case object Kill extends Exception
 
-//  def onHalt[F[_], O](f: Throwable => ExtendableIO[F, O]): ExtendableIO[F, O] = this match {
-//    //case Halt(e) => Try(f(e))
-//    case Emit(h,t)=>Emit(h,t.onHalt(f))
-//  }
+
 }
