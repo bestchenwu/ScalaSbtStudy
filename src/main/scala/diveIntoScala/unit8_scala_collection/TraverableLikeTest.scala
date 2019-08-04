8accdc1937f9986b6843bb951282feab5ca70891
bestchenwu <bestchenwu@163.com>
2019-08-04 19:49:26 +0800

    提交scala集合库的学习

--- src/main/scala/diveIntoScala/unit8_scala_collection/TraverableLikeTest.scala
+++ src/main/scala/diveIntoScala/unit8_scala_collection/TraverableLikeTest.scala
@@ -3,12 +3,15 @@ package diveIntoScala.unit8_scala_collection
 object TraverableLikeTest {
 
   def main(args: Array[String]): Unit = {
-      val list = List(1,2,3,4).toIterator
-      //println(list.drop(2))
-      val (iter1,iter2) = list.partition(_%2==0)
-      while(iter1.hasNext){
-        println(iter1.next())
-      }
-      val set = Set(12,3,4,5,6,7)
+//      val list = List(1,2,3,4).toIterator
+//      //println(list.drop(2))
+//      val (iter1,iter2) = list.partition(_%2==0)
+//      while(iter1.hasNext){
+//        println(iter1.next())
+//      }
+//      val set = Set(12,3,4,5,6,7)
+        val seq1 = Seq(1,2,34,5)
+        val seq2 = Seq(5,6,7,5)
+        println(seq1.diff(seq2))
   }
 }
